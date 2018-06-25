74ea6cd88ebaa350301ef462e736798b52f7eedc
Ilana Kantorov <ilanak@microsoft.com>
2018-06-25 19:15:24 +0300

    change MaxConcurrentCalls = 10

--- Program.cs
+++ Program.cs
@@ -102,7 +102,7 @@ namespace sbqueuelistenerack
             {
                 // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
                 // Set it according to how many messages the application wants to process in parallel.
-                //MaxConcurrentCalls = 20,
+                MaxConcurrentCalls = 10,
 
                 // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
                 // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
